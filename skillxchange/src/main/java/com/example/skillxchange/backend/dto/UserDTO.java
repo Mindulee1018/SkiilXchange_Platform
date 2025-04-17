7833ee8386b8a68bdef85ffd2f29dc9f383b5cfa
Mindulee1018 <fonsekamindu@gmail.com>
2025-04-17 11:08:21 +0530

    add validations

--- skillxchange/src/main/java/com/example/skillxchange/backend/dto/UserDTO.java
+++ skillxchange/src/main/java/com/example/skillxchange/backend/dto/UserDTO.java
@@ -11,6 +11,7 @@ public class UserDTO {
     private String username;
 
     @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
 
     @NotBlank(message = "Password is required")
--- skillxchange/src/main/java/com/example/skillxchange/backend/model/User.java
+++ skillxchange/src/main/java/com/example/skillxchange/backend/model/User.java
@@ -18,6 +18,7 @@ public class User {
     private String username;
 
     @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
 
     @NotBlank(message = "Password is required")
