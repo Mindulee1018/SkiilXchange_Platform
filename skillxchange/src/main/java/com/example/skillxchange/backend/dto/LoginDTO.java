9612df6b24930bac615181debd13c6a1f0c0b236
Mindulee1018 <fonsekamindu@gmail.com>
2025-04-19 13:00:39 +0530

    remove unused lombok.data library

--- skillxchange/src/main/java/com/example/skillxchange/backend/dto/LoginDTO.java
+++ skillxchange/src/main/java/com/example/skillxchange/backend/dto/LoginDTO.java
@@ -2,7 +2,6 @@ package com.example.skillxchange.backend.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
 public class LoginDTO {
     @Email(message = "Invalid email format")
