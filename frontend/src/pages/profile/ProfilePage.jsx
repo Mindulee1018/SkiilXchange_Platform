005a57e432d9b7df359ea3a0da0f4b631311ca4b
Mindulee1018 <fonsekamindu@gmail.com>
2025-05-18 07:20:53 +0530

    handle css

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -897,8 +897,6 @@ const ProfilePage = () => {
                 post={selectedPost}
               />
             )}
-            </>
-            )}
 
             {activeTab === "posts" && (
             <>
